92934b88b78190e4e5351d2416b34650585d98f5
Daniele <danixgithub1@gmail.com>
2023-07-27 01:18:36 +0200

    [STYLE]: added a comment

--- src/main/java/game/controller/engine/api/GameEngine.java
+++ src/main/java/game/controller/engine/api/GameEngine.java
@@ -1,5 +1,9 @@
 package game.controller.engine.api;
 
+/*
+ * to create only once, and then start / kill thread whenever app switch to game scene
+ */
+
 public interface GameEngine {
     void startThread();
 
