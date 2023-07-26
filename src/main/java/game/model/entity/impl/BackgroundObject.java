4b6750abfd72da6c1c641a23a087ee5dd0c682b0
Daniele <danixgithub1@gmail.com>
2023-07-26 21:22:08 +0200

    [CHECKSTYLE]: formatting fixes

--- src/main/java/game/model/entity/impl/BackgroundObject.java
+++ src/main/java/game/model/entity/impl/BackgroundObject.java
@@ -4,10 +4,10 @@ import java.util.Optional;
 
 import game.utility.Rectangle;
 
-public class BackgroundObject extends GameObjectImpl{
+public class BackgroundObject extends GameObjectImpl {
 
     public BackgroundObject(final Rectangle initialPosition, final Optional<String> spriteFileName) {
         super(initialPosition, Optional.empty(), spriteFileName);
     }
-    
+
 }
