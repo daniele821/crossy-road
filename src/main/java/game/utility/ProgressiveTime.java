b580ecac8731a8b37ea55f07d2c993073603ad3f
Daniele <danixgithub1@gmail.com>
2023-07-26 20:29:53 +0200

    [ADD]: ProgressiveTime

---
+++ src/main/java/game/utility/ProgressiveTime.java
@@ -0,0 +1,20 @@
+package game.utility;
+
+public class ProgressiveTime {
+    private final long timeMillisec;
+    private final long deltaTime;
+
+    public ProgressiveTime(final long timeMillisec, final long deltaTime) {
+        this.timeMillisec = timeMillisec;
+        this.deltaTime = deltaTime;
+    }
+
+    public long getTimeMillisec() {
+        return this.timeMillisec;
+    }
+
+    public long getDeltaTime() {
+        return this.deltaTime;
+    }
+
+}
