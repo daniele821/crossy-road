bd1271b6a13ee5acb85860e61e9adf42a2873758
Daniele <danixgithub1@gmail.com>
2023-07-27 15:00:04 +0200

    [ADD]: equals, hashCode to rectangle & vector2D

--- src/main/java/game/utility/Vector2D.java
+++ src/main/java/game/utility/Vector2D.java
@@ -22,4 +22,34 @@ public class Vector2D {
         return "Vector2D [x=" + x + ", y=" + y + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vector2D other = (Vector2D) obj;
+        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) {
+            return false;
+        }
+        return Double.doubleToLongBits(y) == Double.doubleToLongBits(other.y);
+    }
+
 }
