a453ba7e5dc907510d9e00b43e065d4168832f33
Daniele <danixgithub1@gmail.com>
2023-07-27 15:54:22 +0200

    [REFACTOR]: gameEngineThread class turned into a method

--- src/main/java/game/controller/engine/impl/GameEngineImpl.java
+++ src/main/java/game/controller/engine/impl/GameEngineImpl.java
@@ -7,6 +7,8 @@ import game.controller.engine.api.GameLoop;
 import game.utility.ProgressiveTime;
 
 public class GameEngineImpl implements GameEngine {
+    private static final int FPS = 60;
+    private static final int FRAME_DURATION = 1000 / FPS;
     private final GameLoop gameLoop;
     private boolean isPaused;
     private boolean killThread;
@@ -23,7 +25,7 @@ public class GameEngineImpl implements GameEngine {
     @Override
     public void startThread() {
         if (this.engineThread.isEmpty()) {
-            this.engineThread = Optional.ofNullable(new GameEngineThread());
+            this.engineThread = Optional.ofNullable(getGameEngineThread(FRAME_DURATION));
             this.engineThread.get().start();
         }
     }
@@ -67,36 +69,31 @@ public class GameEngineImpl implements GameEngine {
         this.killThread = killThread;
     }
 
-    private class GameEngineThread extends Thread {
-        private static final int FPS = 60;
-        private static final int FRAME_DURATION = 1000 / FPS;
+    private Thread getGameEngineThread(final int frameDuration) {
+        return new Thread(() -> {
+            long totalTime = 0;
+            long elapsedTime;
+            long startTime = System.currentTimeMillis();
+            long lastTime;
 
-        private GameEngineThread() {
-            super(() -> {
-                long totalTime = 0;
-                long elapsedTime;
-                long startTime = System.currentTimeMillis();
-                long lastTime;
+            while (!isThreadKilled()) {
+                lastTime = startTime;
+                startTime = System.currentTimeMillis();
+                elapsedTime = startTime - lastTime;
 
-                while (!isThreadKilled()) {
-                    lastTime = startTime;
-                    startTime = System.currentTimeMillis();
-                    elapsedTime = startTime - lastTime;
-
-                    if (!isPaused()) {
-                        totalTime += elapsedTime;
-                        gameLoop.processInput();
-                        gameLoop.update(new ProgressiveTime(totalTime, elapsedTime));
-                        gameLoop.render();
-                    }
-
-                    try {
-                        sleep(Long.max(0, startTime + FRAME_DURATION - System.currentTimeMillis()));
-                    } catch (final InterruptedException e) {
-                        throw new IllegalStateException("engine sleep failed!", e);
-                    }
+                if (!isPaused()) {
+                    totalTime += elapsedTime;
+                    gameLoop.processInput();
+                    gameLoop.update(new ProgressiveTime(totalTime, elapsedTime));
+                    gameLoop.render();
                 }
-            });
-        }
+
+                try {
+                    Thread.sleep(Long.max(0, startTime + frameDuration - System.currentTimeMillis()));
+                } catch (final InterruptedException e) {
+                    throw new IllegalStateException("engine sleep failed!", e);
+                }
+            }
+        });
     }
 }
