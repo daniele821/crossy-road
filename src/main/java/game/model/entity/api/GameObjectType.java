97a750e74085e584d4e2b15811f47f0dbc589095
Daniele <danixgithub1@gmail.com>
2023-07-27 14:01:33 +0200

    [STYLE]: added a todo comment

--- src/main/java/game/model/entity/api/GameObjectType.java
+++ src/main/java/game/model/entity/api/GameObjectType.java
@@ -1,5 +1,10 @@
 package game.model.entity.api;
 
 public interface GameObjectType {
-    
+    /*
+     * TODO:
+     * 
+     * - add a system to differentiate objects 
+     * - add methods to identify object qualities 
+     */
 }
