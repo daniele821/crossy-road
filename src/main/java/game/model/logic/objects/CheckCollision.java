49314a6e1e8996745a97f3f782f698ad9ef70e8b
Daniele <danixgithub1@gmail.com>
2023-07-29 21:08:37 +0200

    [ADD]: classes to handle the logic of the game

---
+++ src/main/java/game/model/logic/objects/CheckCollision.java
@@ -0,0 +1,7 @@
+package game.model.logic.objects;
+
+import game.model.logic.GameLogicObject;
+
+public interface CheckCollision extends GameLogicObject {
+
+}
---
+++ src/main/java/game/model/logic/objects/PowerupHandler.java
@@ -0,0 +1,7 @@
+package game.model.logic.objects;
+
+import game.model.logic.GameLogicObject;
+
+public interface PowerupHandler extends GameLogicObject {
+
+}
