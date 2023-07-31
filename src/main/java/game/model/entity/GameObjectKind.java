8d666d2504ce1f90c4b9a86194d66df317a82dfa
Daniele <danixgithub1@gmail.com>
2023-07-31 18:57:29 +0200

    added some player images and types

--- src/main/java/game/model/entity/GameObjectKind.java
+++ src/main/java/game/model/entity/GameObjectKind.java
@@ -1,6 +1,7 @@
 package game.model.entity;
 
 public enum GameObjectKind {
+    BACKGROUND,
     PLAYER,
     ENEMY,
     OBSTACLE,
--- src/main/java/game/model/entity/GameObjectType.java
+++ src/main/java/game/model/entity/GameObjectType.java
@@ -1,7 +1,14 @@
 package game.model.entity;
 
+import static game.model.entity.GameObjectKind.*;
+
 public enum GameObjectType {
-    ;
+    PL_DEFAULT("game/entity/default.png", PLAYER),
+    PL_HULK("game/entity/hulk.png", PLAYER),
+    PL_SPONGEBOB("game/entity/spongebob.png", PLAYER),
+    PL_STEVE("game/entity/steve.png", PLAYER),
+    PL_SUPERMAN("game/entity/superman.png", PLAYER),
+    PL_THIEF("game/entity/thief.png", PLAYER);
 
     private final String imageClassPath;
     private final GameObjectKind objectKind;
