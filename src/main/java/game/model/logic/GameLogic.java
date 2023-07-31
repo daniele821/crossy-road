451183184f880b473ccc90c86f7b8b9f00c5291e
Daniele <danixgithub1@gmail.com>
2023-07-31 13:45:29 +0200

    [ADD]: api methods in GameLogic

--- src/main/java/game/model/logic/GameLogic.java
+++ src/main/java/game/model/logic/GameLogic.java
@@ -1,5 +1,14 @@
 package game.model.logic;
 
+import game.model.entity.GameObject;
+import game.shared.ProgressiveTime;
+import game.shared.Vector2D;
+
+// handle coins, powerups, collisions inside the two methods!
+
 public interface GameLogic {
+    void updateAll(ProgressiveTime elapsedTime);
+
+    void movePlayer(GameObject gameObject, Vector2D direction);
 
 }
--- src/main/java/game/model/logic/GameLogicImpl.java
+++ src/main/java/game/model/logic/GameLogicImpl.java
@@ -1,12 +1,23 @@
 package game.model.logic;
 
+import game.model.entity.GameObject;
 import game.model.world.GameWorld;
 import game.shared.AbstractGameWorldUser;
+import game.shared.ProgressiveTime;
+import game.shared.Vector2D;
 
 public class GameLogicImpl extends AbstractGameWorldUser implements GameLogic {
 
     public GameLogicImpl(final GameWorld gameWorld) {
         super(gameWorld);
+    }
+
+    @Override
+    public void updateAll(final ProgressiveTime elapsedTime) {
+    }
+
+    @Override
+    public void movePlayer(final GameObject gameObject, final Vector2D direction) {
     }
 
 }
