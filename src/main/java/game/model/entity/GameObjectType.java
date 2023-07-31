d50781a7e02cc7afb325fdb0e5726fa9888dc131
Daniele <danixgithub1@gmail.com>
2023-07-31 18:00:58 +0200

    [ADD]: added string field (classpath) in GameObjectType

--- src/main/java/game/model/entity/GameObjectType.java
+++ src/main/java/game/model/entity/GameObjectType.java
@@ -1,5 +1,16 @@
 package game.model.entity;
 
 public enum GameObjectType {
+    ;
+
+    private final String imageClassPath;
+
+    private GameObjectType(final String imageClassPath) {
+        this.imageClassPath = imageClassPath;
+    }
+
+    public String getImageClassPath() {
+        return this.imageClassPath;
+    }
 
 }
