75ce6020830537230d47845bbbe5ec5f1fb5cb47
Daniele <danixgithub1@gmail.com>
2023-07-31 15:35:46 +0200

    [ADD]: method to move based on speed and elapsed time

--- src/main/java/game/shared/Algorithms.java
+++ src/main/java/game/shared/Algorithms.java
@@ -43,4 +43,8 @@ public class Algorithms {
         return new Vector2D(vector.getX() * factor, vector.getY() * factor);
     }
 
+    public Rectangle move(final Rectangle rect, final Vector2D speed, final ProgressiveTime elapsedTime) {
+        return add(rect, multiply(speed, (double) elapsedTime.getDeltaTime() / 1000.0));
+    }
+
 }
