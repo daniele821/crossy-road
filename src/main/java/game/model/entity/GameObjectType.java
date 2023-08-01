5e89b422e33ce2e93536469bb62f9acde1d8d5ef
Daniele <danixgithub1@gmail.com>
2023-08-01 15:57:09 +0200

    [RENAMED]: game objects

--- src/main/java/game/model/entity/GameObjectType.java
+++ src/main/java/game/model/entity/GameObjectType.java
@@ -3,21 +3,21 @@ package game.model.entity;
 import static game.model.entity.GameObjectKind.*;
 
 public enum GameObjectType {
-    BG_BIKE_LANE("game/entity/background/bike_lane.png", BACKGROUND),
-    BG_GRASS("game/entity/background/grass.png", BACKGROUND),
-    BG_RAIL("game/entity/background/rail.png", BACKGROUND),
-    BG_ROAD("game/entity/background/road.png", BACKGROUND),
-    BG_WATER("game/entity/background/water.png", BACKGROUND),
-    CO_COIN("game/entity/coin/coin.png", COIN),
-    PL_DEFAULT("game/entity/player/default.png", PLAYER),
-    PL_HULK("game/entity/player/hulk.png", PLAYER),
-    PL_SPONGEBOB("game/entity/player/spongebob.png", PLAYER),
-    PL_STEVE("game/entity/player/steve.png", PLAYER),
-    PL_SUPERMAN("game/entity/player/superman.png", PLAYER),
-    PL_THIEF("game/entity/player/thief.png", PLAYER),
-    PO_IMMORTALITY("game/entity/powerup/immortality.png", POWERUP),
-    PO_MAGNET("game/entity/powerup/magnet.png", POWERUP),
-    PO_MULTIPLIER("game/entity/powerup/multiplier.png", POWERUP);
+    BAC_BIKE_LANE("game/entity/background/bike_lane.png", BACKGROUND),
+    BAC_GRASS("game/entity/background/grass.png", BACKGROUND),
+    BAC_RAIL("game/entity/background/rail.png", BACKGROUND),
+    BAC_ROAD("game/entity/background/road.png", BACKGROUND),
+    BAC_WATER("game/entity/background/water.png", BACKGROUND),
+    COI_COIN("game/entity/coin/coin.png", COIN),
+    PLA_DEFAULT("game/entity/player/default.png", PLAYER),
+    PLA_HULK("game/entity/player/hulk.png", PLAYER),
+    PLA_SPONGEBOB("game/entity/player/spongebob.png", PLAYER),
+    PLA_STEVE("game/entity/player/steve.png", PLAYER),
+    PLA_SUPERMAN("game/entity/player/superman.png", PLAYER),
+    PLA_THIEF("game/entity/player/thief.png", PLAYER),
+    POW_IMMORTALITY("game/entity/powerup/immortality.png", POWERUP),
+    POW_MAGNET("game/entity/powerup/magnet.png", POWERUP),
+    POW_MULTIPLIER("game/entity/powerup/multiplier.png", POWERUP);
 
     private final String imageClassPath;
     private final GameObjectKind objectKind;
