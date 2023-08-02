9591ecfaa5e350d2099f917c3cec2d7d36881ba3
Daniele <danixgithub1@gmail.com>
2023-08-02 14:52:51 +0200

    [REMOVE]: method to store input

--- src/main/java/game/model/logic/GameLogic.java
+++ src/main/java/game/model/logic/GameLogic.java
@@ -1,16 +1,11 @@
 package game.model.logic;
 
-import java.util.function.Consumer;
-
-import game.model.entity.GameWorld;
 import game.shared.ProgressiveTime;
 
 // handle coins, powerups, collisions inside the two methods!
 
 public interface GameLogic {
     void updateAll(ProgressiveTime elapsedTime);
-
-    void storeInputAction(Consumer<GameWorld> action);
 
     void executeInputAction();
 }
--- src/main/java/game/model/logic/GameLogicImpl.java
+++ src/main/java/game/model/logic/GameLogicImpl.java
@@ -1,8 +1,5 @@
 package game.model.logic;
 
-import java.util.Optional;
-import java.util.function.Consumer;
-
 import game.model.entity.GameWorld;
 import game.shared.Algorithms;
 import game.shared.ProgressiveTime;
@@ -10,7 +7,6 @@ import game.shared.ProgressiveTime;
 public class GameLogicImpl implements GameLogic {
     private static final Algorithms ALGORITHMS = new Algorithms();
     private final GameWorld gameWorld;
-    private Optional<Consumer<GameWorld>> storedAction = Optional.empty();
 
     public GameLogicImpl(final GameWorld gameWorld) {
         this.gameWorld = gameWorld;
@@ -22,16 +18,8 @@ public class GameLogicImpl implements GameLogic {
     }
 
     @Override
-    public void storeInputAction(final Consumer<GameWorld> action) {
-        if (this.storedAction.isEmpty()) {
-            this.storedAction = Optional.ofNullable(action);
-        }
-    }
-
-    @Override
     public void executeInputAction() {
-        this.storedAction.ifPresent(action -> action.accept(this.gameWorld));
-        this.storedAction = Optional.empty();
+
     }
 
 }
