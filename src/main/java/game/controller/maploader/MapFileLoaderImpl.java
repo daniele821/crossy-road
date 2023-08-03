6e78f9a1444aab4edf0104289335a3cfee4393c1
Daniele <danixgithub1@gmail.com>
2023-08-03 13:04:21 +0200

    [REMOVE]: hashMap in MapFileLoader

--- src/main/java/game/controller/maploader/MapFileLoaderImpl.java
+++ src/main/java/game/controller/maploader/MapFileLoaderImpl.java
@@ -4,31 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MapFileLoaderImpl implements MapFileLoader {
-    private final Map<String, List<String>> maps = new HashMap<>();
 
     @Override
     public List<String> load(final String mapPath) {
-        if (!this.maps.containsKey(mapPath)) {
-            final var input = ClassLoader.getSystemResourceAsStream(mapPath);
-            if (input == null) {
-                return List.of();
-            }
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
-                final List<String> lines = new ArrayList<>();
-                while (reader.ready()) {
-                    lines.add(reader.readLine());
-                }
-                this.maps.put(mapPath, lines);
-            } catch (final IOException e) {
-                throw new IllegalStateException("map " + mapPath + " failed loading", e);
-            }
+        final var input = ClassLoader.getSystemResourceAsStream(mapPath);
+        if (input == null) {
+            return List.of();
         }
-        return this.maps.get(mapPath);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+            final List<String> lines = new ArrayList<>();
+            while (reader.ready()) {
+                lines.add(reader.readLine());
+            }
+            return lines;
+        } catch (final IOException e) {
+            throw new IllegalStateException("map " + mapPath + " failed loading", e);
+        }
     }
 
 }
