f2e356542d4c0f2268e49b05900cd85fdf7a2ecb
Daniele <danixgithub1@github.com>
2023-08-04 17:26:01 +0200

    [REFACTOR]: removed getters in enums

--- src/main/java/game/model/entity/GameObjectType.java
+++ src/main/java/game/model/entity/GameObjectType.java
@@ -8,50 +8,49 @@ import static game.model.entity.GameObjectType.GameObjectKind.PLAYER;
 import static game.model.entity.GameObjectType.GameObjectKind.POWERUP;
 
 public enum GameObjectType {
-    BAC_BIKE_LANE("bike_lane.png", BACKGROUND, 16, 16, 0, 0),
-    BAC_GRASS("grass.png", BACKGROUND, 16, 16, 0, 0),
-    BAC_RAIL("rail.png", BACKGROUND, 16, 16, 0, 0),
-    BAC_ROAD("road.png", BACKGROUND, 16, 16, 0, 0),
-    BAC_WATER("water.png", BACKGROUND, 16, 16, 0, 0),
-    COI_DEFAULT("coin.png", COIN, 10, 10, 3, 3),
-    ENE_CAR_1_DX("car_1_dx.png", ENEMY, 14, 8, 1, 4),
-    ENE_CAR_1_SX("car_1_sx.png", ENEMY, 14, 8, 1, 4),
-    ENE_CAR_2_DX("car_2_dx.png", ENEMY, 14, 9, 1, 3),
-    ENE_CAR_2_SX("car_2_sx.png", ENEMY, 14, 8, 1, 4),
-    ENE_CAR_3_DX("car_3_dx.png", ENEMY, 14, 9, 1, 3),
-    ENE_CAR_3_SX("car_3_sx.png", ENEMY, 14, 8, 1, 4),
-    ENE_BIKE_DX("bike_dx.png", ENEMY, 8, 7, 4, 3),
-    ENE_BIKE_SX("bike_sx.png", ENEMY, 8, 7, 4, 7),
-    ENE_TRAIN_2_DX("train_2_dx.png", ENEMY, 32, 13, 0, 1),
-    ENE_TRAIN_2_SX("train_2_sx.png", ENEMY, 32, 13, 0, 1),
-    ENE_TRAIN_3_DX("train_3_dx.png", ENEMY, 48, 13, 0, 1),
-    ENE_TRAIN_3_SX("train_3_sx.png", ENEMY, 48, 13, 0, 1),
-    ENE_TRAIN_4_DX("train_4_dx.png", ENEMY, 64, 13, 0, 1),
-    ENE_TRAIN_4_SX("train_4_sx.png", ENEMY, 64, 13, 0, 1),
-    ENE_TRAIN("train.png", ENEMY, 80, 13, 0, 1),
-    OBS_BUSH("bush.png", OBSTACLE, 12, 12, 2, 2),
-    OBS_TREE("tree.png", OBSTACLE, 13, 14, 2, 1),
-    PLA_DEFAULT("default.png", PLAYER, 10, 14, 3, 1),
-    PLA_HULK("hulk.png", PLAYER, 10, 14, 3, 1),
-    PLA_SPONGEBOB("spongebob.png", PLAYER, 12, 13, 2, 2),
-    PLA_STEVE("steve.png", PLAYER, 8, 14, 4, 1),
-    PLA_SUPERMAN("superman.png", PLAYER, 10, 14, 3, 1),
-    PLA_THIEF("thief.png", PLAYER, 10, 14, 3, 1),
-    POW_IMMORTALITY("immortality.png", POWERUP, 10, 10, 3, 3),
-    POW_MAGNET("magnet.png", POWERUP, 12, 10, 2, 3),
-    POW_MULTIPLIER("multiplier.png", POWERUP, 10, 10, 3, 3);
+    BAC_BIKE_LANE("game/entity/background/bike_lane.png", BACKGROUND, 16, 16, 0, 0),
+    BAC_GRASS("game/entity/background/grass.png", BACKGROUND, 16, 16, 0, 0),
+    BAC_RAIL("game/entity/background/rail.png", BACKGROUND, 16, 16, 0, 0),
+    BAC_ROAD("game/entity/background/road.png", BACKGROUND, 16, 16, 0, 0),
+    BAC_WATER("game/entity/background/water.png", BACKGROUND, 16, 16, 0, 0),
+    COI_DEFAULT("game/entity/coin/coin.png", COIN, 10, 10, 3, 3),
+    ENE_CAR_1_DX("game/entity/enemy/car_1_dx.png", ENEMY, 14, 8, 1, 4),
+    ENE_CAR_1_SX("game/entity/enemy/car_1_sx.png", ENEMY, 14, 8, 1, 4),
+    ENE_CAR_2_DX("game/entity/enemy/car_2_dx.png", ENEMY, 14, 9, 1, 3),
+    ENE_CAR_2_SX("game/entity/enemy/car_2_sx.png", ENEMY, 14, 8, 1, 4),
+    ENE_CAR_3_DX("game/entity/enemy/car_3_dx.png", ENEMY, 14, 9, 1, 3),
+    ENE_CAR_3_SX("game/entity/enemy/car_3_sx.png", ENEMY, 14, 8, 1, 4),
+    ENE_BIKE_DX("game/entity/enemy/bike_dx.png", ENEMY, 8, 7, 4, 3),
+    ENE_BIKE_SX("game/entity/enemy/bike_sx.png", ENEMY, 8, 7, 4, 7),
+    ENE_TRAIN_2_DX("game/entity/enemy/train_2_dx.png", ENEMY, 32, 13, 0, 1),
+    ENE_TRAIN_2_SX("game/entity/enemy/train_2_sx.png", ENEMY, 32, 13, 0, 1),
+    ENE_TRAIN_3_DX("game/entity/enemy/train_3_dx.png", ENEMY, 48, 13, 0, 1),
+    ENE_TRAIN_3_SX("game/entity/enemy/train_3_sx.png", ENEMY, 48, 13, 0, 1),
+    ENE_TRAIN_4_DX("game/entity/enemy/train_4_dx.png", ENEMY, 64, 13, 0, 1),
+    ENE_TRAIN_4_SX("game/entity/enemy/train_4_sx.png", ENEMY, 64, 13, 0, 1),
+    ENE_TRAIN("game/entity/enemy/train.png", ENEMY, 80, 13, 0, 1),
+    OBS_BUSH("game/entity/obstacle/bush.png", OBSTACLE, 12, 12, 2, 2),
+    OBS_TREE("game/entity/obstacle/tree.png", OBSTACLE, 13, 14, 2, 1),
+    PLA_DEFAULT("game/entity/player/default.png", PLAYER, 10, 14, 3, 1),
+    PLA_HULK("game/entity/player/hulk.png", PLAYER, 10, 14, 3, 1),
+    PLA_SPONGEBOB("game/entity/player/spongebob.png", PLAYER, 12, 13, 2, 2),
+    PLA_STEVE("game/entity/player/steve.png", PLAYER, 8, 14, 4, 1),
+    PLA_SUPERMAN("game/entity/player/superman.png", PLAYER, 10, 14, 3, 1),
+    PLA_THIEF("game/entity/player/thief.png", PLAYER, 10, 14, 3, 1),
+    POW_IMMORTALITY("game/entity/powerup/immortality.png", POWERUP, 10, 10, 3, 3),
+    POW_MAGNET("game/entity/powerup/magnet.png", POWERUP, 12, 10, 2, 3),
+    POW_MULTIPLIER("game/entity/powerup/multiplier.png", POWERUP, 10, 10, 3, 3);
 
-    private final String imagePath;
-    private final GameObjectKind objectKind;
-    private final double imageLenX;
-    private final double imageLenY;
-    private final double deltaX;
-    private final double deltaY;
+    public final String imagePath;
+    public final GameObjectKind objectKind;
+    public final double imageLenX;
+    public final double imageLenY;
+    public final double deltaX;
+    public final double deltaY;
 
-    GameObjectType(final String imagePath, final GameObjectKind objectKind,
-            final double imageLenX, final double imageLenY,
-            final double deltaX, final double deltaY) {
-        this.imagePath = objectKind.getDirPath() + imagePath;
+    GameObjectType(final String imagePath, final GameObjectKind objectKind, final double imageLenX,
+            final double imageLenY, final double deltaX, final double deltaY) {
+        this.imagePath = imagePath;
         this.objectKind = objectKind;
         this.imageLenX = imageLenX;
         this.imageLenY = imageLenY;
@@ -59,47 +58,13 @@ public enum GameObjectType {
         this.deltaY = deltaY;
     }
 
-    public String getImagePath() {
-        return this.imagePath;
-    }
-
-    public GameObjectKind getObjectKind() {
-        return this.objectKind;
-    }
-
-    public double getImageLenX() {
-        return this.imageLenX;
-    }
-
-    public double getImageLenY() {
-        return this.imageLenY;
-    }
-
-    public double getDeltaX() {
-        return this.deltaX;
-    }
-
-    public double getDeltaY() {
-        return this.deltaY;
-    }
-
     public static enum GameObjectKind {
-        BACKGROUND("game/entity/background/"),
-        PLAYER("game/entity/player/"),
-        ENEMY("game/entity/enemy/"),
-        OBSTACLE("game/entity/obstacle/"),
-        TRUNK("game/entity/trunk/"),
-        COIN("game/entity/coin/"),
-        POWERUP("game/entity/powerup/");
-
-        private final String dirPath;
-
-        GameObjectKind(final String dirPath) {
-            this.dirPath = dirPath;
-        }
-
-        public String getDirPath() {
-            return this.dirPath;
-        }
+        BACKGROUND,
+        PLAYER,
+        ENEMY,
+        OBSTACLE,
+        TRUNK,
+        COIN,
+        POWERUP;
     }
 }
--- src/main/java/game/model/entity/GameWorldType.java
+++ src/main/java/game/model/entity/GameWorldType.java
@@ -3,13 +3,9 @@ package game.model.entity;
 public enum GameWorldType {
     MAP_1("1.map");
 
-    private final String mapPath;
+    public final String mapPath;
 
     GameWorldType(final String mapPath) {
         this.mapPath = "game/map/" + mapPath;
-    }
-
-    public String getMapPath() {
-        return this.mapPath;
     }
 }
