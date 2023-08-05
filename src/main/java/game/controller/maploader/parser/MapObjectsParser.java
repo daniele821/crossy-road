85c6e30e440a10741623543e8481f2c3fdf723a6
Daniele <danixgithub1@github.com>
2023-08-05 22:14:56 +0200

    [FEAT]: mapObjectParser (partial and to test)

--- src/main/java/game/controller/maploader/parser/MapObjectsParser.java
+++ src/main/java/game/controller/maploader/parser/MapObjectsParser.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import game.model.entity.GameObject;
+import game.model.entity.GameObjectImpl;
 import game.model.entity.GameObjectType;
+import game.utility.Rectangle;
 import game.utility.Vector2D;
 
 public class MapObjectsParser {
@@ -23,8 +26,29 @@ public class MapObjectsParser {
             case "speed":
                 this.speed = Optional.ofNullable(this.mapParser.parseVector2D(lineSplitted.getB()));
                 break;
+            case "position":
+                for (final var pos : this.mapParser.splitElem(lineSplitted.getB())) {
+                    this.position.add(this.mapParser.parseVector2D(pos));
+                }
+                break;
+            case "pos_cell":
+                // TODO
+                break;
+            case "pos_rect":
+                // TODO
+                break;
+            case "pos_line":
+                // TODO
+                break;
             default:
                 break;
         }
+    }
+
+    public List<GameObject> getObjects() {
+        return this.position.stream()
+                .map(vect -> new Rectangle(vect.getX(), vect.getY(), type.getLenX(), type.getLenY()))
+                .map(rect -> (GameObject) new GameObjectImpl(rect, this.speed.get(), type, true))
+                .toList();
     }
 }
