e5dcb927289c28e77870866b3fa00f57103a82bb
Daniele <danixgithub1@github.com>
2023-08-05 23:30:23 +0200

    [FEAT]: working map loader (to test)

--- src/main/java/game/controller/maploader/MapWorldLoader.java
+++ src/main/java/game/controller/maploader/MapWorldLoader.java
@@ -1,10 +1,9 @@
 package game.controller.maploader;
 
 import java.util.List;
-import java.util.Optional;
 
 import game.model.entity.GameWorld;
 
 public interface MapWorldLoader {
-    Optional<GameWorld> loadWorld(List<String> lines);
+    GameWorld loadWorld(List<String> lines);
 }
--- src/main/java/game/controller/maploader/MapWorldLoaderImpl.java
+++ src/main/java/game/controller/maploader/MapWorldLoaderImpl.java
@@ -1,17 +1,66 @@
 package game.controller.maploader;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Predicate;
+
+import game.controller.maploader.parser.MapInfoParser;
+import game.controller.maploader.parser.MapObjectsParser;
+import game.model.entity.GameObject;
+import game.model.entity.GameObjectType;
 import game.model.entity.GameWorld;
+import game.model.entity.GameWorldImpl;
+import game.model.entity.GameWorldInfo;
 
 public class MapWorldLoaderImpl implements MapWorldLoader {
+    private static final String COMMENT = "#";
+    private static final String OBJECT_LINE = ">";
+    private MapInfoParser mapInfoParser = new MapInfoParser();
+    private MapObjectsParser mapObjectsParser;
+    private GameWorldInfo gameWorldInfo;
+    private List<GameObject> objects = new ArrayList<>();
 
     @Override
-    public Optional<GameWorld> loadWorld(final List<String> lines) {
+    public GameWorld loadWorld(final List<String> lines) {
+        final List<String> clearedLines = lines.stream()
+                .filter(Predicate.not(String::isEmpty))
+                .filter(str -> !str.startsWith(COMMENT))
+                .toList();
 
-        // TODO
+        final List<String> infoLines = clearedLines.stream()
+                .takeWhile(str -> !str.startsWith(OBJECT_LINE))
+                .toList();
 
-        return Optional.empty();
+        infoLines.stream().forEach(str -> mapInfoParser.parseLine(str));
+        this.gameWorldInfo = this.mapInfoParser.getGameWorldInfo();
+
+        List<String> linesLeft = clearedLines.stream()
+                .dropWhile(str -> !str.startsWith(OBJECT_LINE))
+                .toList();
+
+        while (!linesLeft.isEmpty()) {
+            loadObjects(linesLeft);
+            linesLeft = linesLeft.stream()
+                    .skip(1)
+                    .dropWhile(str -> !str.startsWith(OBJECT_LINE))
+                    .toList();
+        }
+
+        return new GameWorldImpl(this.objects, this.gameWorldInfo);
     }
 
+    private void loadObjects(final List<String> linesLeft) {
+        final GameObjectType type = GameObjectType.valueOf(linesLeft.get(0).replace(OBJECT_LINE, "").strip());
+        this.mapObjectsParser = new MapObjectsParser(type, this.gameWorldInfo);
+
+        final List<String> objectLines = linesLeft.stream()
+                .skip(1)
+                .takeWhile(str -> !str.startsWith(OBJECT_LINE))
+                .toList();
+
+        this.mapObjectsParser = new MapObjectsParser(type, this.gameWorldInfo);
+        objectLines.forEach(str -> mapObjectsParser.parseLine(str));
+
+        this.objects.addAll(this.mapObjectsParser.getObjects());
+    }
 }
