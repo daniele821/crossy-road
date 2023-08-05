950c9d0d149720b11d07c876a296aad23d090891
Daniele <danixgithub1@github.com>
2023-08-05 20:49:30 +0200

    small changes

--- src/main/java/game/controller/maploader/MapParser.java
+++ src/main/java/game/controller/maploader/MapParser.java
@@ -7,33 +7,33 @@ import game.utility.Pair;
 import game.utility.Rectangle;
 import game.utility.Vector2D;
 
-public class MapParser {
+class MapParser {
     private static final String EMPTY = "";
     private static final String LINE_SPLITTER = "=";
     private static final String ELEM_SPLITTER = ",";
     private static final String OBJECT_SPLITTER = " ";
 
-    public List<String> split(final String str, final String splitter) {
+    List<String> split(final String str, final String splitter) {
         return Arrays.asList(str.split(splitter));
     }
 
-    public Pair<String, String> splitLine(final String str) {
+    Pair<String, String> splitLine(final String str) {
         final var splitted = split(str, LINE_SPLITTER);
         return new Pair<>(splitted.get(0).strip(), splitted.size() <= 1 ? EMPTY : splitted.get(1).strip());
     }
 
-    public List<String> splitElem(final String str) {
+    List<String> splitElem(final String str) {
         return split(str, ELEM_SPLITTER);
     }
 
-    public Vector2D parseVector2D(final String str) {
+    Vector2D parseVector2D(final String str) {
         final var splitted = split(str, OBJECT_SPLITTER);
         final var x = Double.parseDouble(splitted.get(0));
         final var y = Double.parseDouble(splitted.get(1));
         return new Vector2D(x, y);
     }
 
-    public Rectangle parseRectangle(final String str) {
+    Rectangle parseRectangle(final String str) {
         final var splitted = split(str, OBJECT_SPLITTER);
         final var x = Double.parseDouble(splitted.get(0));
         final var y = Double.parseDouble(splitted.get(1));
---
+++ src/main/java/game/controller/maploader/MapReader.java
@@ -0,0 +1,5 @@
+package game.controller.maploader;
+
+class MapReader {
+    
+}
