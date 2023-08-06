59dbe757d9cc0d5cbe6c94d981ee479e1e153e0d
Daniele <danixgithub1@github.com>
2023-08-06 13:20:56 +0200

    [FIX]: simplified ImageLoader

--- src/main/java/game/view/imageloader/ImageLoader.java
+++ src/main/java/game/view/imageloader/ImageLoader.java
@@ -1,8 +1,7 @@
 package game.view.imageloader;
 
 import java.awt.Image;
-import java.util.Optional;
 
 public interface ImageLoader {
-    Optional<Image> load(String classPath);
+    Image load(String classPath);
 }
--- src/main/java/game/view/imageloader/ImageLoaderImpl.java
+++ src/main/java/game/view/imageloader/ImageLoaderImpl.java
@@ -2,10 +2,8 @@ package game.view.imageloader;
 
 import java.awt.Image;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.imageio.ImageIO;
 
@@ -13,22 +11,14 @@ public class ImageLoaderImpl implements ImageLoader {
     private final Map<String, Image> images = new HashMap<>();
 
     @Override
-    public Optional<Image> load(final String classPath) {
+    public Image load(final String classPath) {
         if (!images.containsKey(classPath)) {
             try {
-                final InputStream input = ClassLoader.getSystemResourceAsStream(classPath);
-                if (input == null) {
-                    return Optional.empty();
-                }
-                final Image image = ImageIO.read(input);
-                if (image == null) {
-                    return Optional.empty();
-                }
-                this.images.put(classPath, image);
-            } catch (IOException e) {
+                this.images.put(classPath, ImageIO.read(ClassLoader.getSystemResourceAsStream(classPath)));
+            } catch (final IOException e) {
                 throw new IllegalStateException("image" + classPath + "failed loading!", e);
             }
         }
-        return Optional.ofNullable(this.images.get(classPath));
+        return this.images.get(classPath);
     }
 }
