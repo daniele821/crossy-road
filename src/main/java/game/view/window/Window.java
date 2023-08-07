d057f8b305ff3ac734a018c417d3cf75db112dc5
Daniele <danixgithub1@github.com>
2023-08-07 14:59:35 +0200

    [UNDO]: last commit

--- src/main/java/game/view/window/Window.java
+++ src/main/java/game/view/window/Window.java
@@ -1,7 +1,11 @@
 package game.view.window;
 
+import game.model.entity.GameWorld;
 import game.utility.ProgressiveTime;
 
-public interface Window extends WindowInput {
+public interface Window {
     void update(ProgressiveTime elapsedTime);
+
+    void executeStoredActions(ProgressiveTime elapsedTime, GameWorld gameWorld);
+
 }
