03e85ec8d43dfab9dc409daf162d79750bcfaa35
Daniele <danixgithub1@github.com>
2023-08-08 15:27:53 +0200

    [ADD]: launch app in main

--- src/main/java/game/App.java
+++ src/main/java/game/App.java
@@ -1,10 +1,13 @@
 package game;
 
+import game.view.window.WindowFactoryImpl;
+import static game.view.window.WindowFactory.GraphicalRenderer;
+
 public final class App {
     private App() {
     }
 
     public static void main(final String[] args) {
-
+        new WindowFactoryImpl().createWindow(GraphicalRenderer.SWING);
     }
 }
