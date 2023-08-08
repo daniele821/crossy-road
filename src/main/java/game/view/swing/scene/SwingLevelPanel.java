fdfba4beeab202a1170a86747ca1969b283d6d3b
Daniele <danixgithub1@github.com>
2023-08-08 19:51:52 +0200

    [ADD]: LevelPanel and MenuPanel to be implemented

---
+++ src/main/java/game/view/swing/scene/SwingLevelPanel.java
@@ -0,0 +1,13 @@
+package game.view.swing.scene;
+
+import javax.swing.JPanel;
+
+public class SwingLevelPanel extends JPanel implements SwingPanel {
+    private static final long serialVersionUID = 2115458124524211780L;
+
+    @Override
+    public JPanel getPanel() {
+        return this;
+    }
+
+}
--- src/main/java/game/view/swing/scene/SwingWindow.java
+++ src/main/java/game/view/swing/scene/SwingWindow.java
@@ -6,9 +6,11 @@ import game.view.window.AbstractWindow;
 public class SwingWindow extends AbstractWindow {
     private final SwingMainFrame frame = new SwingMainFrame();
     private final SwingGamePanel gamePanel = new SwingGamePanel();
+    private final SwingMenuPanel menuPanel = new SwingMenuPanel();
+    private final SwingLevelPanel levelPanel = new SwingLevelPanel();
 
     public SwingWindow() {
-        this.frame.setSwingPanel(gamePanel);
+        this.frame.setSwingPanel(this.menuPanel);
     }
 
     @Override
