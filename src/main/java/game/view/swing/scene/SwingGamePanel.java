26bcf1351da171fe6703437b7e00aff1f4c98a84
Daniele <danixgithub1@github.com>
2023-08-08 19:33:56 +0200

    [ADD]: methods to pass gameWorld to GamePanel

--- src/main/java/game/view/swing/scene/SwingGamePanel.java
+++ src/main/java/game/view/swing/scene/SwingGamePanel.java
@@ -1,12 +1,34 @@
 package game.view.swing.scene;
 
+import java.awt.Graphics;
+import java.util.Optional;
+
 import javax.swing.JPanel;
+
+import game.model.entity.GameWorld;
 
 public class SwingGamePanel extends JPanel implements SwingPanel {
     private static final long serialVersionUID = 2115458124524211780L;
+    private transient Optional<GameWorld> gameWorld = Optional.empty();
 
     @Override
     public JPanel getPanel() {
         return this;
     }
+
+    public Optional<GameWorld> getGameWorld() {
+        return this.gameWorld.map(GameWorld::copy);
+    }
+
+    public void setGameWorld(final GameWorld gameWorld) {
+        this.gameWorld = Optional.ofNullable(gameWorld).map(GameWorld::copy);
+    }
+
+    @Override
+    protected void paintComponent(final Graphics drawer) {
+        super.paintComponent(drawer);
+
+        // TODO: implement with a concept of camera
+    }
+
 }
