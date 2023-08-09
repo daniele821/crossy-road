dbd1de29088f7d10ccc1c3855a4b8fd15804f9d5
Daniele <danixgithub1@github.com>
2023-08-09 20:22:17 +0200

    [ADD]: set&get for window&frame in panel

--- src/main/java/game/view/swing/panel/SwingPanel.java
+++ src/main/java/game/view/swing/panel/SwingPanel.java
@@ -2,6 +2,17 @@ package game.view.swing.panel;
 
 import javax.swing.JPanel;
 
+import game.view.swing.frame.SwingFrame;
+import game.view.swing.window.SwingWindow;
+
 public interface SwingPanel {
     JPanel getPanel();
+
+    SwingWindow getWindow();
+
+    void setWindow(SwingWindow window);
+
+    SwingFrame getFrame();
+
+    void setFrame(SwingFrame frame);
 }
--- src/main/java/game/view/swing/panel/SwingPanelImpl.java
+++ src/main/java/game/view/swing/panel/SwingPanelImpl.java
@@ -2,12 +2,37 @@ package game.view.swing.panel;
 
 import javax.swing.JPanel;
 
+import game.view.swing.frame.SwingFrame;
+import game.view.swing.window.SwingWindow;
+
 public class SwingPanelImpl extends JPanel implements SwingPanel {
     private static final long serialVersionUID = 2115458124524211780L;
+    private SwingWindow window;
+    private SwingFrame frame;
 
     @Override
     public JPanel getPanel() {
         return this;
+    }
+
+    @Override
+    public SwingWindow getWindow() {
+        return this.window;
+    }
+
+    @Override
+    public void setWindow(final SwingWindow window) {
+        this.window = window;
+    }
+
+    @Override
+    public SwingFrame getFrame() {
+        return this.frame;
+    }
+
+    @Override
+    public void setFrame(final SwingFrame frame) {
+        this.frame = frame;
     }
 
 }
