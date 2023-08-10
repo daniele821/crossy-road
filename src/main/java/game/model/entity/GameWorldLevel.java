14f32fc5d4d0cb1e02005ea5cf931ca174b711ee
Daniele <danixgithub1@github.com>
2023-08-10 20:19:07 +0200

    [FEAT]: implemented GameLevelPanel

--- src/main/java/game/model/entity/GameWorldLevel.java
+++ src/main/java/game/model/entity/GameWorldLevel.java
@@ -1,16 +1,22 @@
 package game.model.entity;
 
 public enum GameWorldLevel {
-    MAP_1("1.map");
+    MAP_1("1.map", "1");
 
     private static final String MAP_DIR = "game/map/";
     private final String path;
+    private final String levelName;
 
-    GameWorldLevel(final String path) {
+    GameWorldLevel(final String path, final String levelName) {
         this.path = MAP_DIR + path;
+        this.levelName = levelName;
     }
 
     public String getPath() {
         return this.path;
+    }
+
+    public String getLevelName() {
+        return this.levelName;
     }
 }
--- src/main/java/game/view/toolkit/swing/panel/SwingLevelPanelImpl.java
+++ src/main/java/game/view/toolkit/swing/panel/SwingLevelPanelImpl.java
@@ -1,6 +1,36 @@
 package game.view.toolkit.swing.panel;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JButton;
+
+import game.model.entity.GameWorldLevel;
+import game.utility.Pair;
+
 public class SwingLevelPanelImpl extends SwingPanelImpl implements SwingLevelPanel {
     private static final long serialVersionUID = 2115458124524211780L;
+    private final List<Pair<GameWorldLevel, JButton>> levelButtons = new ArrayList<>();
+    private final JButton quitButton = new JButton("QUIT");
+
+    @Override
+    public void start() {
+        super.start();
+
+        this.quitButton.addActionListener(e -> getSwingFrame().closeApplication());
+        getJPanel().add(this.quitButton);
+
+        Arrays.asList(GameWorldLevel.values()).stream()
+                .map(level -> new Pair<>(level, new JButton(level.getLevelName())))
+                .peek(this::addActionListener)
+                .peek(this.levelButtons::add)
+                .map(Pair::getB)
+                .forEach(getJPanel()::add);
+    }
+
+    private void addActionListener(final Pair<GameWorldLevel, JButton> pair) {
+        // TODO
+    }
 
 }
