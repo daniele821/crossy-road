719f55b8c89084d22ff0e995e0451cfd8eb96058
Daniele <danixgithub1@github.com>
2023-08-10 13:48:12 +0200

    [IMPL]: SwingWindow

--- src/main/java/game/view/swing/window/SwingWindow.java
+++ src/main/java/game/view/swing/window/SwingWindow.java
@@ -1,13 +1,24 @@
 package game.view.swing.window;
 
+import java.awt.Toolkit;
+
 import game.utility.ProgressiveTime;
+import game.view.swing.frame.SwingFrame;
+import game.view.swing.frame.SwingFrameImpl;
+import game.view.swing.panel.SwingMenuPanelImpl;
 import game.view.window.AbstractWindow;
 
 public class SwingWindow extends AbstractWindow {
+    private final SwingFrame frame = new SwingFrameImpl();
+
+    public SwingWindow() {
+        this.frame.setSwingPanel(new SwingMenuPanelImpl());
+    }
 
     @Override
     public void update(final ProgressiveTime elapsedTime) {
-        // TODO
+        frame.getFrame().repaint();
+        Toolkit.getDefaultToolkit().sync();
     }
 
 }
