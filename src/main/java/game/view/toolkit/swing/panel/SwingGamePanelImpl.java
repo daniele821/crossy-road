3c319465defd6869043b3fc89fc3d9285f168bd8
Daniele <danixgithub1@github.com>
2023-08-11 14:29:14 +0200

    [FEAT]: camera now draws

--- src/main/java/game/view/toolkit/swing/panel/SwingGamePanelImpl.java
+++ src/main/java/game/view/toolkit/swing/panel/SwingGamePanelImpl.java
@@ -1,10 +1,13 @@
 package game.view.toolkit.swing.panel;
 
+import java.awt.Graphics;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import game.controller.engine.GameEngine;
-import game.view.toolkit.swing.camera.Camera;
+import game.model.entity.GameObjectType.GameObjectKind;
 import game.view.toolkit.swing.camera.BasicCamera;
+import game.view.toolkit.swing.camera.Camera;
 
 public class SwingGamePanelImpl extends SwingPanelImpl implements SwingGamePanel {
     private static final long serialVersionUID = 2115458124524211780L;
@@ -27,6 +30,17 @@ public class SwingGamePanelImpl extends SwingPanelImpl implements SwingGamePanel
     @Override
     public void setCamera(final Camera camera) {
         this.camera = Optional.ofNullable(camera);
+    }
+
+    @Override
+    protected void paintComponent(final Graphics drawer) {
+        super.paintComponent(drawer);
+        final var world = getSwingFrame().getCurrentWorld();
+        final var pov = IntStream.range(0, world.getObjects().size())
+                .filter(i -> world.getObjects().get(i).getObjectType().getKind() == GameObjectKind.PLAYER)
+                .mapToObj(Integer::valueOf)
+                .toList();
+        this.camera.get().draw(drawer, world, pov);
     }
 
 }
