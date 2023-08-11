4a8a5d59f98999b268e7cad07ff71dadebaef4eb
Daniele <danixgithub1@github.com>
2023-08-11 21:14:19 +0200

    [FEAT]: PlayerCamera separate all drawAreas!!!

--- src/main/java/game/view/toolkit/swing/camera/PlayerCamera.java
+++ src/main/java/game/view/toolkit/swing/camera/PlayerCamera.java
@@ -1,6 +1,7 @@
 package game.view.toolkit.swing.camera;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.util.List;
 
@@ -31,20 +32,26 @@ public class PlayerCamera implements Camera {
     }
 
     public void draw(final Rectangle drawArea, final Graphics drawer, final GameWorld world, final int objectId) {
+        final Graphics2D drawer2D = (Graphics2D) drawer;
         final GameObject object = WORLD_UTIL.getPresentObject(objectId, world).get();
         final Rectangle objectPos = ALGORITHMS.multiply(object.getPosition(), FACTOR);
         final double objectToBorderOriz = (drawArea.getLenX() - objectPos.getLenX()) / 2;
         final double objectToBorderVert = (drawArea.getLenY() - objectPos.getLenY()) / 2;
         final double leftCorner = objectToBorderOriz - objectPos.getX();
         final double topCorner = objectToBorderVert - objectPos.getY();
+        final int drawAreaX = (int) drawArea.getX();
+        final int drawAreaY = (int) drawArea.getY();
+        final int drawAreaLenX = (int) drawArea.getLenX();
+        final int drawAreaLenY = (int) drawArea.getLenY();
+        drawer2D.clip(new java.awt.Rectangle(drawAreaX, drawAreaY, drawAreaLenX, drawAreaLenY));
         WORLD_UTIL.getPresentObjects(world).forEach(obj -> {
             final int x = (int) (FACTOR * (obj.getPosition().getX()) + leftCorner + drawArea.getX());
             final int y = (int) (FACTOR * (obj.getPosition().getY()) + topCorner + drawArea.getY());
             final int lenX = (int) (FACTOR * obj.getPosition().getLenX());
             final int lenY = (int) (FACTOR * obj.getPosition().getLenY());
             final Image image = this.imageLoader.loadImage(obj.getObjectType().getPath(), lenX, lenY);
-            drawer.drawImage(image, x, y, null);
+            drawer2D.drawImage(image, x, y, null);
         });
+        drawer2D.setClip(null);
     }
-
 }
