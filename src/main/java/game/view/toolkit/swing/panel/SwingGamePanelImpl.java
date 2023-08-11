06cca4ac2e832b0bca86dd68fe7e4391531133b6
Daniele <danixgithub1@github.com>
2023-08-11 15:51:32 +0200

    [FIX]: SwingGamePanel now uses GameWorldUtil

--- src/main/java/game/view/toolkit/swing/panel/SwingGamePanelImpl.java
+++ src/main/java/game/view/toolkit/swing/panel/SwingGamePanelImpl.java
@@ -2,15 +2,18 @@ package game.view.toolkit.swing.panel;
 
 import java.awt.Graphics;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 import game.controller.engine.GameEngine;
 import game.model.entity.GameObjectType.GameObjectKind;
+import game.model.entity.util.GameWorldUtil;
+import game.model.entity.util.GameWorldUtilImpl;
+import game.utility.Pair;
 import game.view.toolkit.swing.camera.BasicCamera;
 import game.view.toolkit.swing.camera.Camera;
 
 public class SwingGamePanelImpl extends SwingPanelImpl implements SwingGamePanel {
     private static final long serialVersionUID = 2115458124524211780L;
+    private static final GameWorldUtil WORLD_UTIL = new GameWorldUtilImpl();
     private transient Optional<Camera> camera = Optional.ofNullable(new BasicCamera());
 
     @Override
@@ -36,9 +39,9 @@ public class SwingGamePanelImpl extends SwingPanelImpl implements SwingGamePanel
     protected void paintComponent(final Graphics drawer) {
         super.paintComponent(drawer);
         final var world = getSwingFrame().getCurrentWorld();
-        final var pov = IntStream.range(0, world.getObjects().size())
-                .filter(i -> world.getObjects().get(i).getObjectType().getKind() == GameObjectKind.PLAYER)
-                .mapToObj(Integer::valueOf)
+        final var pov = WORLD_UTIL.getPresentObjectsId(world).stream()
+                .filter(pair -> pair.getB().getObjectType().getKind() == GameObjectKind.PLAYER)
+                .map(Pair::getA)
                 .toList();
         this.camera.get().draw(drawer, world, pov);
     }
