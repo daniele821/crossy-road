9c49b87f5d045d38215599baeb5c1aa039418ad2
Daniele <danixgithub1@github.com>
2023-08-11 15:38:03 +0200

    [FIX]: GameLogic to use GameWorldUtil

--- src/main/java/game/model/logic/GameLogicImpl.java
+++ src/main/java/game/model/logic/GameLogicImpl.java
@@ -1,26 +1,29 @@
 package game.model.logic;
 
-import java.util.stream.IntStream;
+import java.util.Optional;
 
+import game.model.entity.GameObject;
 import game.model.entity.GameWorld;
+import game.model.entity.util.GameWorldUtil;
+import game.model.entity.util.GameWorldUtilImpl;
 import game.utility.Algorithms;
 import game.utility.Pair;
 import game.utility.ProgressiveTime;
+import game.utility.Rectangle;
 import game.utility.Vector2D;
 
 public class GameLogicImpl implements GameLogic {
     private static final Algorithms ALGORITHMS = new Algorithms();
+    private static final GameWorldUtil WORLD_UTIL = new GameWorldUtilImpl();
 
     @Override
     public void moveObject(final int objectId, final Vector2D movement, final GameWorld gameWorld) {
-        if (objectId >= gameWorld.getObjects().size() || objectId < 0) {
+        final Optional<GameObject> objectOpt = WORLD_UTIL.modifyPresentObjectSafely(objectId, gameWorld);
+        if (objectOpt.isEmpty() || movement.equals(new Vector2D(0, 0))) {
             return;
         }
-        final var object = gameWorld.modifyObjects().get(objectId);
-        if (movement.equals(new Vector2D(0, 0)) || !object.isPresent()) {
-            return;
-        }
-        final var newPos = ALGORITHMS.add(object.getPosition(), movement);
+        final GameObject object = objectOpt.get();
+        final Rectangle newPos = ALGORITHMS.add(object.getPosition(), movement);
         object.setPosition(newPos);
 
         // TODO collisions
@@ -28,9 +31,9 @@ public class GameLogicImpl implements GameLogic {
 
     @Override
     public void updateAll(final ProgressiveTime elapsedTime, final GameWorld gameWorld) {
-        final var objects = gameWorld.getObjects();
-        IntStream.range(0, objects.size())
-                .mapToObj(i -> new Pair<>(i, ALGORITHMS.moveVector(objects.get(i), elapsedTime)))
+        WORLD_UTIL.getPresentObjectsWithId(gameWorld)
+                .stream()
+                .map(pair -> new Pair<>(pair.getA(), ALGORITHMS.moveVector(pair.getB(), elapsedTime)))
                 .forEach(pair -> moveObject(pair.getA(), pair.getB(), gameWorld));
     }
 
