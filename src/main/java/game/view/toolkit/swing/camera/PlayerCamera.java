820ce7ff247a6c80fe4884d0787170afb75bb3b5
Daniele <danixgithub1@github.com>
2023-08-13 00:47:09 +0200

    yippie!!! Camera definetely fixed!!!

--- src/main/java/game/view/toolkit/swing/camera/PlayerCamera.java
+++ src/main/java/game/view/toolkit/swing/camera/PlayerCamera.java
@@ -32,6 +32,8 @@ public class PlayerCamera implements Camera {
     public void draw(final Rectangle drawArea, final Graphics2D drawer2D,
             final GameWorld world, final int objectId, final double factor) {
 
+        // center object
+        final GameWorldInfo info = world.getGameWorldInfo();
         final GameObject object = WORLD_UTIL.getPresentObject(objectId, world).get();
         final Rectangle objectPos = ALGORITHMS.multiply(object.getPosition(), factor);
         final double objectToBorderOriz = (drawArea.getLenX() - objectPos.getLenX()) / 2;
@@ -39,9 +41,27 @@ public class PlayerCamera implements Camera {
         final double leftCorner = objectToBorderOriz - objectPos.getX();
         final double topCorner = objectToBorderVert - objectPos.getY();
 
+        // offset camera to avoid going outside map (ORIZ)
+        final double rightMap = info.getWorldBounds().getX() + info.getWorldBounds().getLenX();
+        final double rightObj = object.getPosition().getX() + object.getPosition().getLenX();
+        final double right = (rightMap - rightObj) * factor;
+        final double leftMap = info.getWorldBounds().getX();
+        final double leftObj = object.getPosition().getX();
+        final double left = (leftMap - leftObj) * factor;
+        final double offsetOriz = Double.max(0, objectToBorderOriz - right) - Double.max(0, objectToBorderOriz + left);
+
+        // offset camera to avoid going outside map (VERT)
+        final double downMap = info.getWorldBounds().getY() + info.getWorldBounds().getLenY();
+        final double downObj = object.getPosition().getY() + object.getPosition().getLenY();
+        final double down = (downMap - downObj) * factor;
+        final double upMap = info.getWorldBounds().getY();
+        final double upObj = object.getPosition().getY();
+        final double up = (upMap - upObj) * factor;
+        final double offsetVert = Double.max(0, objectToBorderVert - down) - Double.max(0, objectToBorderVert + up);
+
         WORLD_UTIL.getPresentObjects(world).forEach(obj -> {
-            final int x = (int) (factor * (obj.getPosition().getX()) + leftCorner + drawArea.getX());
-            final int y = (int) (factor * (obj.getPosition().getY()) + topCorner + drawArea.getY());
+            final int x = (int) (factor * (obj.getPosition().getX()) + leftCorner + drawArea.getX() + offsetOriz);
+            final int y = (int) (factor * (obj.getPosition().getY()) + topCorner + drawArea.getY() + offsetVert);
             final int lenX = 1 + (int) (factor * obj.getPosition().getLenX());
             final int lenY = 1 + (int) (factor * obj.getPosition().getLenY());
             final Image image = this.imageLoader.loadImage(obj.getObjectType().getPath(), lenX, lenY);
