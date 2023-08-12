71afa004086b9ab05719e40df8c44d52887f32d0
Daniele <danixgithub1@github.com>
2023-08-12 12:21:41 +0200

    [ADD]: constructors in camera impl for scale factor

--- src/main/java/game/view/toolkit/swing/camera/BasicCamera.java
+++ src/main/java/game/view/toolkit/swing/camera/BasicCamera.java
@@ -11,18 +11,22 @@ import game.view.toolkit.swing.imageloader.ImageBufferedLoader;
 import game.view.toolkit.swing.imageloader.ImageBufferedLoaderImpl;
 
 public class BasicCamera implements Camera {
-    private static final double FACTOR = 5;
     private static final GameWorldUtil WORLD_UTIL = new GameWorldUtilImpl();
     private final ImageBufferedLoader imageLoader = new ImageBufferedLoaderImpl();
+    private final double factor;
+
+    public BasicCamera(final double factor) {
+        this.factor = factor;
+    }
 
     @Override
     public void draw(final Rectangle drawArea, final Graphics2D drawer2D, final GameWorld world, final int objectId) {
         WORLD_UTIL.getPresentObjects(world).forEach(obj -> {
             final var info = world.getGameWorldInfo();
-            final int x = (int) (FACTOR * (obj.getPosition().getX() - info.getWorldBounds().getX()));
-            final int y = (int) (FACTOR * (obj.getPosition().getY() - info.getWorldBounds().getY()));
-            final int lenX = (int) (FACTOR * obj.getPosition().getLenX());
-            final int lenY = (int) (FACTOR * obj.getPosition().getLenY());
+            final int x = (int) (factor * (obj.getPosition().getX() - info.getWorldBounds().getX()));
+            final int y = (int) (factor * (obj.getPosition().getY() - info.getWorldBounds().getY()));
+            final int lenX = (int) (factor * obj.getPosition().getLenX());
+            final int lenY = (int) (factor * obj.getPosition().getLenY());
             final Image image = this.imageLoader.loadImage(obj.getObjectType().getPath(), lenX, lenY);
             drawer2D.drawImage(image, (int) (x + drawArea.getX()), (int) (y + drawArea.getY()), null);
         });
--- src/main/java/game/view/toolkit/swing/camera/PlayerCamera.java
+++ src/main/java/game/view/toolkit/swing/camera/PlayerCamera.java
@@ -13,28 +13,31 @@ import game.view.toolkit.swing.imageloader.ImageBufferedLoader;
 import game.view.toolkit.swing.imageloader.ImageBufferedLoaderImpl;
 
 public class PlayerCamera implements Camera {
-    private static final double FACTOR = 8;
     private static final GameWorldUtil WORLD_UTIL = new GameWorldUtilImpl();
     private static final Algorithms ALGORITHMS = new Algorithms();
     private final ImageBufferedLoader imageLoader = new ImageBufferedLoaderImpl();
+    private final double factor;
+
+    public PlayerCamera(final double factor) {
+        this.factor = factor;
+    }
 
     @Override
     public void draw(final Rectangle drawArea, final Graphics2D drawer2D, final GameWorld world, final int objectId) {
         final GameObject object = WORLD_UTIL.getPresentObject(objectId, world).get();
-        final Rectangle objectPos = ALGORITHMS.multiply(object.getPosition(), FACTOR);
+        final Rectangle objectPos = ALGORITHMS.multiply(object.getPosition(), factor);
         final double objectToBorderOriz = (drawArea.getLenX() - objectPos.getLenX()) / 2;
         final double objectToBorderVert = (drawArea.getLenY() - objectPos.getLenY()) / 2;
         final double leftCorner = objectToBorderOriz - objectPos.getX();
         final double topCorner = objectToBorderVert - objectPos.getY();
 
         WORLD_UTIL.getPresentObjects(world).forEach(obj -> {
-            final int x = (int) (FACTOR * (obj.getPosition().getX()) + leftCorner + drawArea.getX());
-            final int y = (int) (FACTOR * (obj.getPosition().getY()) + topCorner + drawArea.getY());
-            final int lenX = (int) (FACTOR * obj.getPosition().getLenX());
-            final int lenY = (int) (FACTOR * obj.getPosition().getLenY());
+            final int x = (int) (factor * (obj.getPosition().getX()) + leftCorner + drawArea.getX());
+            final int y = (int) (factor * (obj.getPosition().getY()) + topCorner + drawArea.getY());
+            final int lenX = (int) (factor * obj.getPosition().getLenX());
+            final int lenY = (int) (factor * obj.getPosition().getLenY());
             final Image image = this.imageLoader.loadImage(obj.getObjectType().getPath(), lenX, lenY);
             drawer2D.drawImage(image, x, y, null);
         });
-        drawer2D.setClip(null);
     }
 }
