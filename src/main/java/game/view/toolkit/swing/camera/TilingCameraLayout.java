13aad2ddea2a2b806012f7ecd3518e7ecc2c4a51
Daniele <danixgithub1@github.com>
2023-08-15 00:05:12 +0200

    [FEAT]: RECT_FILL_EMPTY_SPOT case implemented

--- src/main/java/game/view/toolkit/swing/camera/TilingCameraLayout.java
+++ src/main/java/game/view/toolkit/swing/camera/TilingCameraLayout.java
@@ -1,6 +1,7 @@
 package game.view.toolkit.swing.camera;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -19,7 +20,8 @@ public class TilingCameraLayout extends AbstractCameraLayout {
         VERTICAL,
         ORIZONTAL,
         VERT_ORIZ,
-        OPTIMAL;
+        RECT_WITH_EMPTY_SPOT,
+        RECT_FILL_EMPTY_SPOT;
     }
 
     @Override
@@ -33,10 +35,42 @@ public class TilingCameraLayout extends AbstractCameraLayout {
             case VERTICAL -> splitOrizontallyWithBorders(borderLessRect, objects.size());
             case ORIZONTAL -> splitVerticallyWithBorders(borderLessRect, objects.size());
             case VERT_ORIZ -> splitVertOrizWithBorder(borderLessRect, objects.size());
-            // TODO implement all cases
+            case RECT_WITH_EMPTY_SPOT -> getAreas(borderLessRect, objects.size(), false);
+            case RECT_FILL_EMPTY_SPOT -> getAreas(borderLessRect, objects.size(), true);
             default -> throw new UnsupportedOperationException(this.layout + " layout not supported!");
         };
 
         IntStream.range(0, objects.size()).forEach(i -> CAMERA.draw(areas.get(i), drawer, world, objects.get(i)));
+    }
+
+    private List<Rectangle> getAreas(final Rectangle area, final int n, final boolean fillEmptyAreas) {
+        final double factorSides = area.getLenX() / area.getLenY();
+        final double factorFixed = Double.max(1 / (double) n, Double.min(n, factorSides));
+        final double num1 = Math.sqrt((double) n / factorFixed);
+        final double num2 = (double) n / num1;
+        final int min = (int) Double.min(num1, num2);
+        final int max = (n / min) * min < n ? n / min + 1 : n / min;
+        final int vert = num1 > num2 ? min : max;
+        final int oriz = num1 > num2 ? max : min;
+        if (!fillEmptyAreas) {
+            return splitOrizontallyWithBorders(area, oriz).stream()
+                    .flatMap(rect -> splitVerticallyWithBorders(rect, vert).stream())
+                    .toList();
+        }
+        final int emptyAreas = vert * oriz - n;
+        final List<Integer> nAreas = new ArrayList<>();
+        IntStream.range(0, oriz).forEach(i -> {
+            nAreas.add(oriz - i <= emptyAreas ? vert - 1 : vert);
+        });
+        return getAreas(area, nAreas);
+    }
+
+    private List<Rectangle> getAreas(final Rectangle area, final List<Integer> n) {
+        final List<Rectangle> orizSplit = splitOrizontallyWithBorders(area, n.size());
+        final List<Rectangle> res = new ArrayList<>();
+        IntStream.range(0, n.size()).forEach(i -> {
+            res.addAll(splitVerticallyWithBorders(orizSplit.get(i), n.get(i)));
+        });
+        return res;
     }
 }
