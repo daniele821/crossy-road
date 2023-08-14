6a94154b1b194ea7334f6f834364ddae670c9616
Daniele <danixgithub1@github.com>
2023-08-14 18:59:21 +0200

    [ADD]: methods to split vertically and orizontally with borders

--- src/main/java/game/view/toolkit/swing/camera/AbstractCameraLayout.java
+++ src/main/java/game/view/toolkit/swing/camera/AbstractCameraLayout.java
@@ -3,11 +3,13 @@ package game.view.toolkit.swing.camera;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import game.model.entity.GameWorld;
 import game.model.entity.util.GameWorldUtil;
 import game.model.entity.util.GameWorldUtilImpl;
 import game.utility.Algorithms;
+import game.utility.Rectangle;
 
 public abstract class AbstractCameraLayout implements CameraLayout {
     protected static final GameWorldUtil WORLD_UTIL = new GameWorldUtilImpl();
@@ -21,5 +23,31 @@ public abstract class AbstractCameraLayout implements CameraLayout {
         if (objectId.stream().filter(id -> !WORLD_UTIL.isObjectIdValid(id, world)).count() != 0) {
             throw new IllegalArgumentException("objectId (" + objectId + ") is not valid!");
         }
+    }
+
+    protected Rectangle removeBorders(final Rectangle area) {
+        final double x = area.getX() + BORDER_WIDTH;
+        final double y = area.getY() + BORDER_WIDTH;
+        final double lenX = area.getLenX() - 2 * BORDER_WIDTH;
+        final double lenY = area.getLenY() - 2 * BORDER_WIDTH;
+        return new Rectangle(x, y, lenX, lenY);
+    }
+
+    protected List<Rectangle> splitVerticallyWithBorders(final Rectangle area, final int n) {
+        final double widthTot = area.getLenX();
+        final double widthTotBorder = widthTot - (n - 1) * BORDER_WIDTH;
+        final double width = widthTotBorder / n;
+        return IntStream.range(0, n)
+                .mapToObj(i -> new Rectangle(width * i, area.getY(), width, area.getLenY()))
+                .toList();
+    }
+
+    protected List<Rectangle> splitOrizontallyWithBorders(final Rectangle area, final int n) {
+        final double heightTot = area.getLenY();
+        final double heightTotBorder = heightTot - (n - 1) * BORDER_WIDTH;
+        final double height = heightTotBorder / n;
+        return IntStream.range(0, n)
+                .mapToObj(i -> new Rectangle(area.getX(), height * i, area.getLenX(), height))
+                .toList();
     }
 }
