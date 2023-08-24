a91d60b5b83d9e5b8ba694ca005ee39e7f776e1d
D. Muffato <danixgithub1@gmail.com>
2023-08-24 15:24:06 +0200

    LAST COMMIT: this project will be ended here, as it completed most of the things it needed to do, and i am getting bored!

--- src/main/java/game/model/logic/MovementLogicImpl.java
+++ src/main/java/game/model/logic/MovementLogicImpl.java
@@ -1,5 +1,6 @@
 package game.model.logic;
 
+import java.util.List;
 import java.util.Optional;
 
 import game.model.entity.GameObject;
@@ -7,6 +8,7 @@ import game.model.entity.GameObjectType.GameObjectKind;
 import game.model.entity.GameWorld;
 import game.model.entity.GameWorldUtil;
 import game.model.entity.GameWorldUtilImpl;
+import game.utility.Pair;
 import game.utility.Rectangle;
 import game.utility.Vector2D;
 
@@ -61,15 +63,35 @@ public class MovementLogicImpl implements MovementLogic {
         if (object.getObjectType().getKind() != GameObjectKind.PLAYER) {
             return true;
         }
-        return this.worldUtil.getColliding(world, objectId, newPos)
-                .stream()
-                .filter(pair -> pair.getB().getObjectType().getKind() == GameObjectKind.OBSTACLE)
-                .count() == 0;
+        final var info = world.getGameWorldInfo();
+        final var type = object.getObjectType();
+        final var roundedPos = this.worldUtil.roundPosToCellPos(newPos, info, type);
+        final var colliding = this.worldUtil.getColliding(world, objectId, roundedPos);
+        return !contains(GameObjectKind.OBSTACLE, colliding) && !contains(GameObjectKind.PLAYER, colliding);
     }
 
     @Override
     public void postMove(final GameWorld world, final int objectId, final GameObject object, final Rectangle newPos) {
-        // TODO
+        final var colliding = this.worldUtil.getColliding(world, newPos)
+                .stream()
+                .filter(pair -> pair.getB().getObjectType().getKind() != GameObjectKind.BACKGROUND)
+                .toList();
+        if (contains(GameObjectKind.PLAYER, colliding)) {
+            colliding.stream()
+                    .filter(pair -> pair.getB().getObjectType().getKind() != GameObjectKind.PLAYER)
+                    .forEach(pair -> {
+                        switch (pair.getB().getObjectType().getKind()) {
+                            case COIN:
+                                pair.getB().setPresent(false);
+                                break;
+                            default:
+                                break;
+                        }
+                    });
+        }
     }
 
+    private boolean contains(final GameObjectKind kind, final List<Pair<Integer, GameObject>> objects) {
+        return objects.stream().filter(pair -> pair.getB().getObjectType().getKind() == kind).count() != 0;
+    }
 }
