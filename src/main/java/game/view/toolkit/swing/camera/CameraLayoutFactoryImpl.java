2f9af9310dd7882b6f41c1c63f10a8c860e038df
D. Muffato <danixgithub1@gmail.com>
2023-08-24 13:48:22 +0200

    [CHANGE]: default camera layout

--- src/main/java/game/view/toolkit/swing/camera/CameraLayoutFactoryImpl.java
+++ src/main/java/game/view/toolkit/swing/camera/CameraLayoutFactoryImpl.java
@@ -4,7 +4,7 @@ import game.model.entity.GameWorld;
 import game.view.toolkit.swing.camera.TilingCameraLayout.Layout;
 
 public class CameraLayoutFactoryImpl implements CameraLayoutFactory {
-    private static final Layout DEFAULT_LAYOUT = Layout.RECT_FILL_EMPTY_SPOT;
+    private static final Layout DEFAULT_LAYOUT = Layout.RECT_WITH_EMPTY_SPOT;
 
     @Override
     public CameraLayout create(final Layout layout, final GameWorld world) {
