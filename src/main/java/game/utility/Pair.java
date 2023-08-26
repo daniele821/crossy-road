09fb67fb58aa9ec0a35ddcbaa983e27b3fcfd2cb
D. Muffato <danixgithub1@gmail.com>
2023-08-26 19:22:44 +0200

    added pair class

---
+++ src/main/java/game/utility/Pair.java
@@ -0,0 +1,25 @@
+package game.utility;
+
+public class Pair<A, B> {
+    private final A a;
+    private final B b;
+
+    public Pair(final A a, final B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [a=" + a + ", b=" + b + "]";
+    }
+
+}
