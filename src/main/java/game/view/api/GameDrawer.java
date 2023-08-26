1c1c7710ac7681f2662919007bfa7c96ab3e1c40
D. Muffato <danixgithub1@gmail.com>
2023-08-27 00:04:50 +0200

    add method to start drawer

--- src/main/java/game/view/api/GameDrawer.java
+++ src/main/java/game/view/api/GameDrawer.java
@@ -4,6 +4,21 @@ import game.model.api.GameWorldDrawable;
 
 /** rendered for the game world. */
 public interface GameDrawer {
+
+    /**
+     * method to start renderer.
+     * <p>
+     * this means:
+     * </p>
+     * <p>
+     * - making the window visible (GUI drawer)
+     * </p>
+     * <p>
+     * - enabling writing on terminal (CLI/TUI drawer)
+     * </p>
+     */
+    void startRenderer();
+
     /**
      * method to draw game world on the specific drawer.
      * 
