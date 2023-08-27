a44c304b0b9fca966b698d3e9b5e753c8e75019e
D. Muffato <danixgithub1@gmail.com>
2023-08-27 14:55:47 +0200

    added factory for api interfaces

---
+++ src/main/java/game/model/api/GameWorldFactory.java
@@ -0,0 +1,12 @@
+package game.model.api;
+
+import game.model.resources.GameWorldLevel;
+
+public interface GameWorldFactory {
+    static GameWorldFactory getDefaultFactory() {
+        // TODO return an implementation when one exists!
+        throw new UnsupportedOperationException("GameWorldFactory hasn't been implemented yet!");
+    }
+
+    GameWorld loadLevel(GameWorldLevel level);
+}
